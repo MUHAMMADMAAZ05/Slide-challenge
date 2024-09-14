589699d22368d3da1c15acf4d256c273689a9ffa
MUHAMMADMAAZ05 <shaikhsiddiqui931@gmail.com>
2024-09-14 17:30:21 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-const car={
-    make: "BRV Hetec",
-    model: "BRV 2",
-    year:  2023,
-    colour: ["red","blue","White","Black"],
-    hybrid: true,
-    drive: function(){
-        console.log("The car has been started")
-    },
-    stop: function(){
-        console.log("The car has been stopped")
-    },
-};
+// const car={
+//     make: "BRV Hetec",
+//     model: "BRV 2",
+//     year:  2023,
+//     colour: ["red","blue","White","Black"],
+//     hybrid: true,
+//     drive: function(){
+//         console.log("The car has been started")
+//     },
+//     stop: function(){
+//         console.log("The car has been stopped")
+//     },
+// }
 
-console.log(car.make)
-console.log(car.colour[0])
-car.drive()
-car.stop()
+// console.log(car.make)
+// console.log(car.colour[0])
+// car.drive()
+// car.stop()
+
+
+const person1={
+    name: "Maaz",
+    age: 19,
+    status: "resident",
+}
+
+const person2={
+    name: "John Doe",
+    age: 39,
+    status: "Tourist",
+}
+
+if ( person1.age > 18 && person1.status=="resident"){
+    console.log("His nationality is Pakitan")
+}
+else{
+    console.log("You are tourist")
+}
+
+if ( person2.age > 18 && person2.status=="resident"){
+    console.log("His nationality is Pakitan")
+}
+else{
+    console.log("You are tourist")
+}
